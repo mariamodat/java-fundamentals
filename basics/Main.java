f9cc7ce4f288269fd88c5f535a9489ef84e74968
mariamodat <mariamodat0@gmail.com>
2021-06-28 21:41:22 +0300

    lab02 almost done

--- basics/Main.java
+++ basics/Main.java
@@ -13,19 +13,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        // here is Lab 01 solution :
-//        Scanner Input = new Scanner(System.in);
-//        System.out.println("Enter a word: ");
-//        String myWord = Input.nextLine();
-//        System.out.println("Enter a number: ");
-//        int myNumber = Input.nextInt();
-//        System.out.println("I own " + myNumber+" "+ Pluralize(myWord , myNumber));
-//
-//        System.out.println("Enter number of heads: ");
-//        int headsNum = Input.nextInt();
-//        flipNHeads(headsNum );
-//
-//        clock();
+         here is Lab 01 solution :
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String myWord = Input.nextLine();
+        System.out.println("Enter a number: ");
+        int myNumber = Input.nextInt();
+        System.out.println("I own " + myNumber+" "+ Pluralize(myWord , myNumber));
+
+        System.out.println("Enter number of heads: ");
+        int headsNum = Input.nextInt();
+        flipNHeads(headsNum );
+
+        clock();
 
 
         // lab 02 solution :
@@ -40,55 +40,55 @@ public class Main {
 
     // 1. function of pluralize the words :
 //
-//    public static String Pluralize(String word, int n) {
-//        if ( n == 0 || n > 1) {
-//            return word + "s";
-//        } else{
-//            return word;
-//        }
-//    }
+    public static String Pluralize(String word, int n) {
+        if ( n == 0 || n > 1) {
+            return word + "s";
+        } else{
+            return word;
+        }
+    }
 
 
     // 2. fuction of the heads and tails
 //
-//    public static void flipNHeads(int n){
-//        Random rand = new Random();
-//        int counter = 0;
-//        int heads = 0;
-//        boolean condition = true;
-//        while(heads != n){
-//            float float_random=rand.nextFloat();
-//            counter++;
-//            if(float_random < 0.5){
-//                System.out.println("tails");
-//                heads = 0;
-//            }else{
-//                System.out.println("heads");
-//                heads++;
-//            }
-//        }
-//        System.out.println("It took "+ counter+" flips to flip "+ n +" in a row.");
-//    }
+    public static void flipNHeads(int n){
+        Random rand = new Random();
+        int counter = 0;
+        int heads = 0;
+        boolean condition = true;
+        while(heads != n){
+            float float_random=rand.nextFloat();
+            counter++;
+            if(float_random < 0.5){
+                System.out.println("tails");
+                heads = 0;
+            }else{
+                System.out.println("heads");
+                heads++;
+            }
+        }
+        System.out.println("It took "+ counter+" flips to flip "+ n +" in a row.");
+    }
 
 
 // 3. fuction of printing the clock each second :
 
-//public static void clock (){
-//    int x= 0 ;
-//int second;
-//    while(true){
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-//
-//        LocalDateTime now = LocalDateTime.now();
-//        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-//        second= now.getSecond();
-//        if ( x != second)
-//        {
-//        System.out.println(time);
-//        x=second;}
-//    }
-//
-//}
+public static void clock (){
+    int x= 0 ;
+int second;
+    while(true){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        LocalDateTime now = LocalDateTime.now();
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        second= now.getSecond();
+        if ( x != second)
+        {
+        System.out.println(time);
+        x=second;}
+    }
+
+}
 
 
     // lab 02 solution :
